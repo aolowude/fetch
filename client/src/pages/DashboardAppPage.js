5db1254e05d1884dea8f1609a982ea7f032ba5bc
aolowude <aolowude@gmail.com>
2023-10-26 15:30:45 +0100

    typo

--- client/src/pages/DashboardAppPage.js
+++ client/src/pages/DashboardAppPage.js
@@ -36,7 +36,7 @@ export default function DashboardAppPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Prodcts Viewed" total={17} icon={'ant-design:android-filled'} />
+            <AppWidgetSummary title="Products Viewed" total={17} icon={'ant-design:android-filled'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
